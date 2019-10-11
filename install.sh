1ef1fa5d04d20ac32768dd62c5fc1a0c9ff36e69
Matt Gerst <mattgerst@gmail.com>
2019-10-10 22:16:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,6 +69,14 @@ else
     echo "[WRN] Could not find nvim, skipping"
 fi
 
+# Awesome Configuration
+if check_executable awesome; then
+    check_directory .config/awesome
+    check_link awesome .config/awesome
+else
+    echo "[WRN] Could not find awesomewm, skipping"
+fi
+
 # Git Template
 if check_executable git; then
     check_link git_template .git_template
