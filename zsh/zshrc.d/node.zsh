aa68b5fc8f202f904061d108402ca8672d61a9aa
Matt Gerst <matt.gerst@interactiveshift.com>
2020-06-26 11:54:53 -0500

    Handle the bin differently

--- zsh/zshrc.d/node.zsh
+++ zsh/zshrc.d/node.zsh
@@ -1,5 +1,7 @@
 # n configuration:
-export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+export N_PREFIX="$HOME/n"
+
+[[ :$PATH: == *"$N_PREFIX/bin:"* ]] || export PATH="$N_PREFIX/bin:$PATH"
 
 if [ -d "$HOME/.npm-global/bin" ]; then
     export PATH="$HOME/.npm-global/bin:$PATH"
