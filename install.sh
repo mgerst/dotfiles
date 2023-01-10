5a6526328692597e93358500c57404b46aa67aad
Matt Gerst <mattgerst@gmail.com>
2023-01-10 09:47:55 -0600

    Remove check_directory call for nvim

--- install.sh
+++ install.sh
@@ -67,7 +67,6 @@ if check_executable nvim; then
         git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
     fi
 
-    check_directory .config/nvim
     check_link nvim .config/nvim
 
     nvim +PackerSync
