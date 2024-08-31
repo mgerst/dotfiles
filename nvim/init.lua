04a4ca60b17e12fd035d9dced8648a5e50c9a990
Matt Gerst <mattgerst@gmail.com>
2024-08-31 10:42:37 -0500

    Switch to raw lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,2 @@
-require("user.autocommands")
-require("user.keybindings")
-
-local treesitter_ok, _ = pcall(require, "nvim-treesitter")
-
-if treesitter_ok then
-  require("user.plugin-configs.treesitter")
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
